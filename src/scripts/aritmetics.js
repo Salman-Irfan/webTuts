812a26f57ff624cea4c01cc8d5b65334b2e7c8b8
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2025-01-25 18:42:00 +0500

    arithmetics in js

---
+++ src/scripts/aritmetics.js
@@ -0,0 +1,12 @@
+console.log(`external script`)
+
+console.log(2+4-7*9/5)
+console.log(2*4)
+console.log(2-4)
+console.log(2/4)
+console.log(2%4)
+
+let ans = 2 + 4 - 7 * 9 / 5
+console.log(ans)
+
+
