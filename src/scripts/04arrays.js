1441d1c1f48c13dfbb65ca195cab22274a4d0a1a
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2025-01-26 19:01:58 +0500

    arrays and objects

---
+++ src/scripts/04arrays.js
@@ -0,0 +1,24 @@
+let names = ["ali", "usman"]
+console.log(names[1])
+console.log(names[2])
+
+
+let student = {
+    "full name": `salman`,
+    email: `salman@gmail.com`,
+    rollNo: 123,
+    institute: `uol`,
+    isTeacher: true,
+    1: `number as key`,
+    2: `2nd num as a key`,
+}
+
+console.log(student)
+console.log(student["full name"])
+console.log(student.email)
+console.log(student.rollNo)
+console.log(student.institute)
+console.log(student.isTeacher)
+
+console.log(student[1])
+console.log(student[1])
