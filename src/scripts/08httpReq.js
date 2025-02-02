f62a192acc08c63222adefd00f1092d48fa10c00
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2025-02-02 18:13:56 +0500

    http req with fetch

---
+++ src/scripts/08httpReq.js
@@ -0,0 +1,13 @@
+const line1 = `this is line 1`
+const line2 = `this is line 2`
+
+console.log(line1)
+console.log(line2)
+
+const fetchGeoData = async () => {
+    const other = await fetch(`https://www.geo.tv/`) // 5s
+    console.log(other)
+}
+
+fetchGeoData()
+
