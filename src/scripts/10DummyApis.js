c74a5236c6deff962451a1d735788e273ff7c445
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2025-02-03 18:49:59 +0500

    text to speech and disctionary app with api

--- src/scripts/10DummyApis.js
+++ src/scripts/10DummyApis.js
@@ -3,6 +3,5 @@ const url = `https://jsonplaceholder.typicode.com/posts` // sent by backend deve
 const getGeoData = async () => {
     const geoRes = await axios.get(url) // 5s
     console.log(geoRes.data)
-    
 }
 getGeoData()
