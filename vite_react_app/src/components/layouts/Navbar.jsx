a37157ad85a1fff0f4ab94160835414419c01d8e
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2025-02-23 19:54:05 +0500

    global state management with redux toolkit

--- vite_react_app/src/components/layouts/Navbar.jsx
+++ vite_react_app/src/components/layouts/Navbar.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const [cartCounter, setCartCounter] = useState(0);
+    const counter = useSelector((state) => state.counter.value)
     return (
         <>
             <nav className='bg-red-500 flex flex-row'>
@@ -11,6 +13,7 @@ const Navbar = () => {
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                     <p>{cartCounter}</p>
+                    <p>{counter}</p>
                 </ul>
             </nav>
         </>
--- vite_react_app/src/components/pages/Contact.jsx
+++ vite_react_app/src/components/pages/Contact.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from '../../reduxGlobalState/slices/counterSlice'
 
 const Contact = () => {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
     return (
-        <div>Contact</div>
+        <>
+            <div>
+                <div>
+                    <button
+                        aria-label="Increment value"
+                        onClick={() => dispatch(increment())}
+                    >
+                        Increment
+                    </button>
+                    <span>{count}</span>
+                    <button
+                        aria-label="Decrement value"
+                        onClick={() => dispatch(decrement())}
+                    >
+                        Decrement
+                    </button>
+                </div>
+            </div>
+        </>
     )
 }
 
